0838b7b89d2c4c5393835945b5b2e2ae71524795
Ann Allan <ann.cristina.allan@gmail.com>
2018-03-02 23:14:35 -0500

    Add Total Calories

--- app.js
+++ app.js
@@ -46,6 +46,17 @@ const ItemCtrl = (function() {
 
       return newItem;
     },
+    getTotalCalories: function(){
+      let total = 0;
+      // Loop through items and add cals
+      data.items.forEach(function(item){
+        total += item.calories;
+      });
+      // Set total cal in data structure
+      data.totalCalories = total;
+      // Return totoal
+      return data.totalCalories;
+    },
     logData: function(){
       return data;
     }
@@ -61,7 +72,8 @@ const UICtrl = (function() {
     itemList: '#item-list',
     addBtn: '.add-btn',
     itemNameInput: '#item-name',
-    itemCaloriesInput: '#item-calories'
+    itemCaloriesInput: '#item-calories',
+    totalCalories: '.total-calories'
   }
   // Pulic methods
   return {
@@ -103,6 +115,9 @@ const UICtrl = (function() {
     hideList: function(){
       document.querySelector(UISelectors.itemList).style.display = 'none';
     },
+    showTotalCalories: function(totalCalories){
+      document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+    },
     getSelectors: function(){
       return UISelectors;
     }
@@ -133,6 +148,11 @@ const App = (function(ItemCtrl, UICtrl) {
 
       // Add item to UI list
       UICtrl.addListItem(newItem);
+
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
       // Clear fields
       UICtrl.clearInput();
     }
@@ -153,6 +173,12 @@ const App = (function(ItemCtrl, UICtrl) {
         // Populate list with getItems
         UICtrl.populateItemList(items);
       }
+
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
+
       // Load event listeners
       loadEventListeners();
     }
